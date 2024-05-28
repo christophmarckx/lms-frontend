8cf87f26e5b6c5f2f8229c0b8f7caef7a3a669b3
BODDAERT SIMON <simon.boddaert@minfin.fed.be>
2024-05-28 10:49:50 +0200

    Adding layout, header, footer & home components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, Routes, withComponentInputBinding} from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {securityInterceptor} from './authentication/security.interceptor';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([securityInterceptor]))
+  ]
 };
