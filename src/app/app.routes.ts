1c893be8779b8252b0ed59a5b32d51aff5c7ee5f
johanvervoort2 <vevoort.johan@gmail.com>
2024-05-29 13:32:31 +0200

    added MOD-3 frontend story

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AddCourseFormComponent} from "./add-course-form/add-course-form.component";
+import {ModuleGalleryComponent} from "./module-gallery/module-gallery.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent },
-  {path: 'add-course', component: AddCourseFormComponent}
+  {path: 'add-course', component: AddCourseFormComponent},
+  {path: 'modules', component: ModuleGalleryComponent}
 
 ];
