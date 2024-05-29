0f2b3c495ffcde67444ee8d7a16c606b770c552d
johanvervoort2 <vevoort.johan@gmail.com>
2024-05-29 11:29:58 +0200

    added MOD-3 frontend

---
+++ src/app/services/module.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Module} from "../models/module";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModuleService {
+
+  private urlString: string;
+
+  constructor(private http: HttpClient) {
+    this.urlString = `${environment.backendUrl}/modules`
+  }
+
+  getAllModules(): Observable<Module[]> {
+    return this.http.get<Module[]>(this.urlString);
+  }
+
+}
