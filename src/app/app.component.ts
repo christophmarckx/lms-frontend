a29fe1b7a4502a2106d6b5ee2b05d5c09002a5bf
Louis Fitdevoie <fitdevoielouis@hotmail.be>
2024-05-30 14:51:35 +0200

    Adding some styles for the homepage/header/footer + adding btns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {LayoutComponent} from "./layout/layout.component";
+import {FooterComponent} from "./layout/footer/footer.component";
+import {HeaderComponent} from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
