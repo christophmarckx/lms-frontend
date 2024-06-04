a80e1d083803e8c9b400cbda7b17ed74ef2a25a6
Berthet M. <78256782+Murmure0@users.noreply.github.com>
2024-06-04 16:48:24 +0200

    Link coach to classgroup : ok, but ugly

---
+++ src/app/services/coach/coach.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AuthenticationService} from "../authentication/authentication.service";
+import {environment} from "../../../environment/environment";
+import {Observable} from "rxjs";
+import {Coach} from "../../model/coach/Coach";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoachService {
+  http = inject(HttpClient);
+  ulrString: string;
+  authenticationService = inject(AuthenticationService);
+  constructor() {
+    this.ulrString = `${environment.backendUrl}/coaches`;
+  }
+
+  getAllCoaches(): Observable<Coach[]> {
+    return this.http.get<Coach[]>(this.ulrString);
+  }
+}
