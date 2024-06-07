96e4fcdafe92d0b052262a5136ae3c08844ecc32
Michael Koene <michael.koene@skynet.be>
2024-06-07 09:47:06 +0200

    Refactor modules list style

--- src/app/component/module-gallery/module-gallery.component.ts
+++ src/app/component/module-gallery/module-gallery.component.ts
@@ -9,6 +9,7 @@ import {ModuleService} from "../../services/module/module.service";
 import { UserRole } from '../../models/authentication/authenticated-user';
 import {Module} from "../../models/module/module";
 import {ModuleWithCourses} from "../../models/module/module-with-courses";
+import {ModuleHierarchy} from "../../models/module/module-hierarchy";
 
 @Component({
   selector: 'app-module-gallery',
@@ -19,7 +20,7 @@ import {ModuleWithCourses} from "../../models/module/module-with-courses";
 })
 export class ModuleGalleryComponent implements OnInit{
   authenticationService: AuthenticationService = inject(AuthenticationService);
-  modules: Module[];
+  moduleHierarchy: ModuleHierarchy[];
   selectedModule: ModuleWithCourses | null;
   moduleService: ModuleService = inject(ModuleService);
   @ViewChild('module-card') moduleId: string = '';
@@ -37,7 +38,7 @@ export class ModuleGalleryComponent implements OnInit{
   }
 
   ngOnInit() {
-    this.moduleService.getAllModules().subscribe(modules => this.modules = modules);
+    this.moduleService.getModuleHierarchy().subscribe(moduleHierarchy => this.moduleHierarchy = moduleHierarchy);
   }
 
   protected readonly UserRole = UserRole;
