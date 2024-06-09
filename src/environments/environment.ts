29248616d798a967af6948aaf9f20ed8163816ac
ahorguelin <122381200+ahorguelin@users.noreply.github.com>
2024-06-09 14:04:31 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
-  backendUrl: 'https://lms-trapezium-backend.onrender.com/',
-  keycloakUrl: 'https://keycloak.switchfully.com/',
-  realm: 'java-2024-02-COTTT',
-  clientId: 'lms',
-  keycloakRedirectUri: 'https://lms-trapezium.vercel.app/',
+  backendUrl: '',
+  keycloakUrl: '',
+  realm: '',
+  clientId: '',
+  keycloakRedirectUri: '',
 };
