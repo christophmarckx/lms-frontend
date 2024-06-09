f76b697d53e760990a88ad6ab029390fedadc657
ahorguelin <antoinehorguelin@gmail.com>
2024-06-09 09:52:26 +0200

    fixing env v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    backendUrl: 'https://lms-trapezium-backend.onrender.com/swagger-ui/index.html',
+    keycloakUrl: 'https://keycloak.switchfully.com/',
+    realm: 'java-2024-02-COTTT',
+    clientId: 'lms',
+};
