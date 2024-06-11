edefadf6df5c8179d2a1f932628a7e7e700eb386
christophmarckx <christoph.marckx@cegeka.com>
2024-06-11 21:24:46 +0200

    changed environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  backendUrl:'https://lms-testing-07c37f7a8e37.herokuapp.com/',
+  backendUrl:'https://lms-testing-07c37f7a8e37.herokuapp.com',
   keycloakUrl:'https://keycloak.switchfully.com/',
   realm:'testing-2024-04',
   clientId:'lms',
