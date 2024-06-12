ceb6c64d2f124c7f98a6880b099bd9b4c640b399
christophmarckx <christoph.marckx@cegeka.com>
2024-06-12 19:49:24 +0200

    changed environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   backendUrl:'https://lms-testing-9224595bf671.herokuapp.com',
-  keycloakUrl:'https://keycloak.switchfully.com/',
+  keycloakUrl:'https://jenkins.switchfully.com/',
   realm:'testing-2024-04',
   clientId:'lms',
   keycloakRedirectUri:'https://switchfully-testing-lms.netlify.app/'
